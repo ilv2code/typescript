7ee6105d4621ce014654e4c8ce1f7d4e7863c0b4
ilv2code <vlive1c@gmail.com>
2018-12-01 11:12:52 +0530

    Abstract Classes......

---
+++ learnabstract.ts
@@ -0,0 +1,46 @@
+//abstract classes cannot be instantiated directly 
+//why..?? may be bcoz these classes which are declared as abstract need not be instantiated
+//these provides some basic set up thats all 
+abstract class Employee{
+    empName:string = "Default!";
+    salary:number = 1010;
+//we dont have a function body and it just say what it should return,
+//defines only how the function should look like
+
+    abstract info(empName:string,salary:number):void;
+
+//for non abstract we need to provide body(functionality)
+display(){
+      return this.empName;
+    }
+}
+//we can only inherite from absract classes....
+//all of the abstract methods are supposed to be defined in derived class --its a must
+class Emp1 extends Employee{
+    info(empName:string,salary:number){
+        this.empName=empName;
+        this.salary=salary;
+        console.log(`${this.empName} & ${this.salary}`);
+     }
+}
+let emp1 = new Emp1();
+console.log(emp1);
+emp1.info("Yashu",50000);
+emp1.info("Sam",20000);
+emp1.info("Ram",30000);
+emp1.info("Geetha",60000);
+//non abstract cannot be over ridden but abstract can be over ridden
+console.log(emp1.display());
+console.log(emp1.display());
+console.log(emp1.display());
+
+
+
+
+
+
+
+
+
+
+
